1f20aba5dd9c17896c31449dc71a2aad7d101e41
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-04-12 17:12:06 +0200

    feat: completed bonus 1

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ console.log(nextElem);
 const prevElem = document.querySelector("div .my-previous");
 console.log(prevElem);
 
+const thumbnailsElem = document.querySelector(".my-thumbnails");
+
 const activeClass = "active";
 
 // inserisco immagini nel DOM
@@ -25,36 +27,71 @@ images.forEach((curElem) => {
     `
     console.log(itemElem);
 
+    const thumbnails = document.createElement("img"); 
+    thumbnails.classList.add("img-fluid", "my-thumbnail");
+    thumbnails.src = `./${curElem.image}`
+    thumbnails.alt = `${curElem.title} picture`
+
     carouselElem.append(itemElem);
+    thumbnailsElem.append(thumbnails);
+
 });
+
 
 // prendo tutte le immagini che ho inserito
 const allCarouselItem = document.querySelectorAll(".my-carousel-item");
 console.log(allCarouselItem);
 
+// prendo tutti i thumbnail
+const allThumbnails = document.querySelectorAll("img.my-thumbnail")
+console.log(allThumbnails);
+
+
+
 let indexItem = 0;
 
-// prendo la prima immagine e aggiungo classe active
+// prendo la prima immagine e la prima thumbnail e aggiungo classe active
 toggleClass(allCarouselItem[indexItem], activeClass);
+
+toggleClass(allThumbnails[indexItem], activeClass);
 
 nextElem.addEventListener("click", () => {
     toggleClass(allCarouselItem[indexItem], activeClass);
+    toggleClass(allThumbnails[indexItem], activeClass);
     indexItem++;
     if(indexItem < allCarouselItem.length) {
-        toggleClass(allCarouselItem[indexItem], activeClass)
+        toggleClass(allCarouselItem[indexItem], activeClass);
+        toggleClass(allThumbnails[indexItem], activeClass);
     } else {
         indexItem = 0;
-        toggleClass(allCarouselItem[indexItem], activeClass)
+        toggleClass(allCarouselItem[indexItem], activeClass);
+        toggleClass(allThumbnails[indexItem], activeClass);
     }
 });
 
 prevElem.addEventListener("click", () => {
     toggleClass(allCarouselItem[indexItem], activeClass);
+    toggleClass(allThumbnails[indexItem], activeClass);
     indexItem--;
     if(indexItem < 0) {
         indexItem = allCarouselItem.length - 1;
-        toggleClass(allCarouselItem[indexItem], activeClass)
+        toggleClass(allCarouselItem[indexItem], activeClass);
+        toggleClass(allThumbnails[indexItem], activeClass);
     } else {
-        toggleClass(allCarouselItem[indexItem], activeClass)
+        toggleClass(allCarouselItem[indexItem], activeClass);
+        toggleClass(allThumbnails[indexItem], activeClass);
     }
+});
+
+// aggiungo event listner a tutte i tuhmbnail
+allThumbnails.forEach((curItem, index) => {
+    curItem.addEventListener("click", () => {
+        toggleClass(allThumbnails[indexItem], activeClass)
+        toggleClass(allCarouselItem[indexItem], activeClass)
+
+        indexItem = index;
+        toggleClass(curItem, activeClass)
+        toggleClass(allCarouselItem[index], activeClass)
+        
+    });
 });
